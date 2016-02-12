fa975ef0706d64c27e6327ffb1bce00ed671b9d0
Jordan Christiansen <xordspar0@gmail.com>
2016-02-11 19:27:54 -0600

    Added Google Map demo
    The map shows and a simple route is displayed.

---
+++ demos/visualization.js
@@ -0,0 +1,29 @@
+var gDirectionsService;
+var gDirectionsDisplay;
+
+function initMap() {
+	gDirectionsService = new google.maps.DirectionsService();
+	gDirectionsDisplay = new google.maps.DirectionsRenderer();
+	var gMap = new google.maps.Map(document.getElementById('map-canvas'), {
+		zoom: 8,
+		center: { lat: -37.811106, lng: 144.962160 }
+	});
+	gDirectionsDisplay.setMap(gMap);
+}
+
+function calcRoute() {
+	var request = {
+		origin: "Ballarat, VIC",
+		destination: "Melbourne, VIC",
+		travelMode: google.maps.TravelMode.DRIVING
+	};
+	gDirectionsService.route(request, function(result, status) {
+		if (status == google.maps.DirectionsStatus.OK) {
+			alert("success");
+			gDirectionsDisplay.setDirections(result);
+		}
+		else {
+			alert("Directions request failed with the error: " + status);
+		}
+	});
+}
