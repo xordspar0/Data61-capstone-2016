24a2727faf6ee066d92e8f05cac9d0a40ee013bf
Jordan Christiansen <xordspar0@gmail.com>
2016-02-19 20:16:09 -0600

    Export KML to a file
    The file doesn't load as-is in Google Earth or National Map. That will
    be done in the next commit or two.

--- exportKML.js
+++ exportKML.js
@@ -1,27 +1,43 @@
-function exportkml(){
-	var kmlns = 'http://www.opengis.net/kml/2.2'
-	var kmlroot = document.implementation.createDocument (kmlns, 'kml', null);
-	var Document = document.createElement('Document');
+function exportKML(){
+	var kmlns = "http://www.opengis.net/kml/2.2"
+	var kmlroot = document.implementation.createDocument (kmlns, "kml", null);
+	var Document = document.createElement("Document");
 	kmlroot.documentElement.appendChild(Document);
-	var Style = document.createElement('Style');
-	Style.setAttribute('id', 'good-route');
-	Document.appendChild(Style);
-	var LineStyle = document.createElement('LineStyle');
-	Style.appendChild(LineStyle);
-	var Color = document.createElement('Color');
-	LineStyle.appendChild(Color);
-	var hexcode1 = document.createTextNode("ff0000ff");
-	Color.appendChild(hexcode1);
-	var Style = document.createElement('Style');
-	Style.setAttribute('id', 'good-route');
-	Document.appendChild(Style);
-	var LineStyle = document.createElement('LineStyle');
-	Style.appendChild(LineStyle);
-	var Color = document.createElement('Color');
-	LineStyle.appendChild(Color);
-	var hexcode1 = document.createTextNode("ff0000ff");
-	Color.appendChild(hexcode1);
+	var styleGoodRoute = document.createElement("Style");
+		styleGoodRoute.setAttribute("id", "good-route");
+		Document.appendChild(styleGoodRoute);
+		var LineStyle = document.createElement("LineStyle");
+		styleGoodRoute.appendChild(LineStyle);
+		var Color = document.createElement("Color");
+		LineStyle.appendChild(Color);
+		var hexcode1 = document.createTextNode("ff00ff00");
+		Color.appendChild(hexcode1);
+	var styleBadRoute = document.createElement("Style");
+		styleBadRoute.setAttribute("id", "bad-route");
+		Document.appendChild(styleBadRoute);
+		var LineStyle = document.createElement("LineStyle");
+		styleBadRoute.appendChild(LineStyle);
+		var Color = document.createElement("Color");
+		LineStyle.appendChild(Color);
+		var hexcode1 = document.createTextNode("ff0000ff");
+		Color.appendChild(hexcode1);
+	var route1 = document.createElement("Placemark");
+		Document.appendChild(route1);
+		var route1Name = document.createElement("name")
+		route1.appendChild(route1Name);
+		var route1NameText = document.createTextNode("mockup");
+		route1Name.appendChild(route1NameText);
+		var route1Style = document.createElement("styleUrl");
+		route1.appendChild(route1Style);
+		var route1StyleURL = document.createTextNode("good-route");
+		route1Style.appendChild(route1StyleURL);
+		var route1LineString = document.createElement("LineString");
+			route1.appendChild(route1LineString);
+			route1Coords = document.createElement("coordinates");
+			route1LineString.appendChild(route1Coords);
+			route1CoordsValues = document.createTextNode("143.841,-37.553,0 144.951,-37.799,0");
+			route1Coords.appendChild(route1CoordsValues);
 	
-	document.write(kmlroot);
-	
+	var s = new XMLSerializer();
+	return s.serializeToString(kmlroot);
 }
--- gAPI.js
+++ gAPI.js
@@ -4,7 +4,7 @@ var gDirectionsDisplay;
 function initMap() {
 	gDirectionsService = new google.maps.DirectionsService();
 	gDirectionsDisplay = new google.maps.DirectionsRenderer();
-	var gMap = new google.maps.Map(document.getElementById('map-canvas'), {
+	var gMap = new google.maps.Map(document.getElementById("map-canvas"), {
 		zoom: 8,
 		center: { lat: -37.811106, lng: 144.962160 }
 	});
@@ -20,8 +20,12 @@ function calcRoute() {
 	gDirectionsService.route(request, function(result, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
 			//console.log(response.routes[0].overview_path[0].lat());
-			exportkml();
-			alert("success");
+			var kmlDoc = exportKML(result);
+			var downloadButton = document.getElementById("download-button");
+			downloadButton.setAttribute("href", "data:application/vnd.google-earth.kml+xml;charset=utf-8,"
+				+ encodeURIComponent(kmlDoc));
+			downloadButton.setAttribute("style", "");
+			
 			gDirectionsDisplay.setDirections(result);
 		}
 		else {
