2e63375890eb5e348fff0554b2a8366dc0b9129f
Joe Osborne <josborne012@gmail.com>
2016-02-23 19:27:09 -0600

    Fixed capitalization bug when generating KML

--- exportKML.js
+++ exportKML.js
@@ -7,42 +7,37 @@ function exportKML(coordPairs) {
 	
 	var kmlns = "http://www.opengis.net/kml/2.2"
 	var kmlroot = document.implementation.createDocument(kmlns, "kml", null);
-	var Document = document.createElement("Document");
+	var Document = document.createElementNS(kmlns, "Document");
 	kmlroot.documentElement.appendChild(Document);
-	var styleGoodRoute = document.createElement("Style");
+	var styleGoodRoute = document.createElementNS(kmlns, "Style");
 		styleGoodRoute.setAttribute("id", "good-route");
 		Document.appendChild(styleGoodRoute);
-		var LineStyle = document.createElement("LineStyle");
+		var LineStyle = document.createElementNS(kmlns, "LineStyle");
 		styleGoodRoute.appendChild(LineStyle);
-		var Color = document.createElement("Color");
+		var Color = document.createElementNS(kmlns, "Color");
 		LineStyle.appendChild(Color);
-		var hexcode1 = document.createTextNode("ff00ff00");
-		Color.appendChild(hexcode1);
-	var styleBadRoute = document.createElement("Style");
+		Color.appendChild(document.createTextNode("ff00ff00"));
+	var styleBadRoute = document.createElementNS(kmlns, "Style");
 		styleBadRoute.setAttribute("id", "bad-route");
 		Document.appendChild(styleBadRoute);
-		var LineStyle = document.createElement("LineStyle");
+		var LineStyle = document.createElementNS(kmlns, "LineStyle");
 		styleBadRoute.appendChild(LineStyle);
-		var Color = document.createElement("Color");
+		var Color = document.createElementNS(kmlns, "Color");
 		LineStyle.appendChild(Color);
-		var hexcode1 = document.createTextNode("ff0000ff");
-		Color.appendChild(hexcode1);
-	var route1 = document.createElement("Placemark");
+		Color.appendChild(document.createTextNode("ff0000ff"));
+	var route1 = document.createElementNS(kmlns, "Placemark");
 		Document.appendChild(route1);
-		var route1Name = document.createElement("name")
+		var route1Name = document.createElementNS(kmlns, "name")
 		route1.appendChild(route1Name);
-		var route1NameText = document.createTextNode("mockup");
-		route1Name.appendChild(route1NameText);
-		var route1Style = document.createElement("styleUrl");
+		route1Name.appendChild(document.createTextNode("mockup"));
+		var route1Style = document.createElementNS(kmlns, "styleUrl");
 		route1.appendChild(route1Style);
-		var route1StyleURL = document.createTextNode("good-route");
-		route1Style.appendChild(route1StyleURL);
-		var route1LineString = document.createElement("LineString");
+		route1Style.appendChild(document.createTextNode("good-route"));
+		var route1LineString = document.createElementNS(kmlns, "LineString");
 			route1.appendChild(route1LineString);
-			route1Coords = document.createElement("coordinates");
+			route1Coords = document.createElementNS(kmlns, "coordinates");
 			route1LineString.appendChild(route1Coords);
-			route1CoordsValues = document.createTextNode(routeCoordString);
-			route1Coords.appendChild(route1CoordsValues);
+			route1Coords.appendChild(document.createTextNode(routeCoordString));
 	
 	var s = new XMLSerializer();
 	return s.serializeToString(kmlroot);
