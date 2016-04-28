8d9b571bcb8ed74a1cf68fb1f89f202fff2f8db3
Rachelle Clayton <rachelle.clayton@ndsu.edu>
2016-04-28 10:49:12 -0500

    Removed routeBoxer

--- gAPI.js
+++ gAPI.js
@@ -2,9 +2,6 @@ var gDirectionsService;
 var gMap;
 var kmlLayer;
 
-var distance = 5;
-var routeBoxer = new RouteBoxer();
-
 var currentRating = "good"; //temporary for demo
 
 function initMap() {
@@ -59,10 +56,6 @@ function calcRoute(request) {
 	gDirectionsService.route(request, function(result, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
 			
-			var path = result.routes[0].overview_path;
-			var boxes = routeBoxer.box(path, distance);
-			drawBoxes(boxes);
-			
 			var coordPairs;
 
 			var numRoutes = result.routes.length;
@@ -99,20 +92,6 @@ function calcRoute(request) {
 			alert("Directions request failed with the error: " + status);
 		}
 	});
-}
-
-function drawBoxes(boxes) {
-	boxpolys = new Array(boxes.length);
-	for (var i = 0; i < boxes.length; i++) {
-		boxpolys[i] = new google.maps.Rectangle({
-				bounds: boxes[i],
-				fillOpacity: 0,
-				strokeOpacity: 1.0,
-				strokeColor: '#000000',
-				storkeWeight: 1,
-				map: gMap
-		});
-	}
 }
 
 function rankArrayByIndex(arrayToSort){
